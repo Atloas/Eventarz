ea88f796c5a49cea374c9deb96e211e0f06db2e3
Atloas <miter010@gmail.com>
2020-12-29 11:46:15 +0100

    Feign Client works

---
+++ Eventarz-FeignClient/src/test/java/com/agh/EventarzFeignClient/EventarzFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.EventarzFeignClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventarzFeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
