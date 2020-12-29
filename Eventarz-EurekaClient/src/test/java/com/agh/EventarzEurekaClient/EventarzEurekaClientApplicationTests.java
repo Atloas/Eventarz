6a07eaef448e8f259b18f6afdd6583e8e5918600
Atloas <miter010@gmail.com>
2020-12-29 11:44:07 +0100

    Eureka Client works

---
+++ Eventarz-EurekaClient/src/test/java/com/agh/EventarzEurekaClient/EventarzEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.EventarzEurekaClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventarzEurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
