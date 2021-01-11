556d5d515fb3701c401ac22f76753b711a0ade5e
Atloas <miter010@gmail.com>
2021-01-11 17:41:39 +0100

    SpringCloudLoadBalancer

---
+++ Eventarz-SpringCloudLoadBalancer/src/test/java/com/agh/EventarzSpringCloudLoadBalancer/EventarzSpringCloudLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.EventarzSpringCloudLoadBalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventarzSpringCloudLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
