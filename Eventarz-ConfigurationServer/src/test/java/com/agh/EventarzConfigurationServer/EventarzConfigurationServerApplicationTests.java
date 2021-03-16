2ac6053758cfe6b93b22da85c4f62a888e95e22d
Atloas <miter010@gmail.com>
2021-03-16 17:06:50 +0100

    Implemented ConfigurationServer

---
+++ Eventarz-ConfigurationServer/src/test/java/com/agh/EventarzConfigurationServer/EventarzConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.EventarzConfigurationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventarzConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
